f4a85ecc1183bf140fbce405e27729619c835ac9
crisog <ortega.cpp@gmail.com>
2021-11-19 02:03:37 -0400

    manual generation

--- cmd/pocket.ts
+++ cmd/pocket.ts
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma";
+// import prisma from "../lib/prisma";
 
 // POKT Price API
 const axios = require("axios");
@@ -21,10 +21,10 @@ const relayToPOKTRatio = 0.01 * 0.89;
 
 const queryApi = influxClient.getQueryApi(influxOrg);
 
-const coin = {
-  name: "pocket",
-  symbol: "POKT",
-};
+// const coin = {
+//   name: "pocket",
+//   symbol: "POKT",
+// };
 
 // Update Pocket Network daily revenue data
 // a cron job should hit this endpoint every hour
@@ -38,8 +38,8 @@ const pocketImport = async () => {
 
   const revenueBlockchains = `["0001","0003","0004","0005","000A","0006","0007","0009","000B","0010","0021","0022","0023","0024","0025","0026","0027","000C","0028", "0040"]`;
 
-  const project = await getProject(coin.name);
-  const lastId = project.lastImportedId;
+  // const project = await getProject(coin.name);
+  const lastId = "1634270400";
   const parsedId = parseInt(lastId);
 
   if (isNaN(parsedId)) {
@@ -57,6 +57,8 @@ const pocketImport = async () => {
     formatDate(fromDate),
     formatDate(toDate)
   );
+
+  let totalRevenue = 0;
 
   for (const day of days) {
     const dayISO = formatDate(day); // YYYY-MM-DD
@@ -109,7 +111,7 @@ const pocketImport = async () => {
     }
 
     console.log(
-      `${project.name} estimated revenue on ${dayISO}: ${revenue.toLocaleString(
+      `Pocket estimated revenue on ${dayISO}: ${revenue.toLocaleString(
         "en-US",
         {
           style: "currency",
@@ -118,94 +120,105 @@ const pocketImport = async () => {
       )} USD.`
     );
 
-    const dateUnixTimestamp = day.getTime() / 1000;
+    // const dateUnixTimestamp = day.getTime() / 1000;
 
-    const fee = {
-      date: dateUnixTimestamp,
-      fees: revenue,
-    };
+    totalRevenue += revenue;
 
-    await storeDBData(fee, project.id);
+    // const fee = {
+    //   date: dateUnixTimestamp,
+    //   fees: revenue,
+    // };
+
+    // await storeDBData(fee, project.id);
   }
 
+  console.log(
+    `Estimated revenue since October 15th: ${totalRevenue.toLocaleString(
+      "en-US",
+      {
+        style: "currency",
+        currency: "USD",
+      }
+    )} USD`
+  );
   console.log("Finished updating pocket revenue...");
 
   return;
 };
 
-const getProject = async (name: string) => {
-  let project = await prisma.project.findFirst({
-    where: {
-      name: name,
-    },
-  });
+// const getProject = async (name: string) => {
+//   let project = await prisma.project.findFirst({
+//     where: {
+//       name: name,
+//     },
+//   });
 
-  if (project == null) {
-    console.log(`Project ${name} doesn't exist. Create it`);
-    await prisma.project.create({
-      data: {
-        name: name,
-        lastImportedId: "1625112000", // July 1st 2021
-      },
-    });
+//   if (project == null) {
+//     console.log(`Project ${name} doesn't exist. Create it`);
+//     await prisma.project.create({
+//       data: {
+//         name: name,
+//         lastImportedId: "1625112000", // July 1st 2021
+//       },
+//     });
 
-    project = await prisma.project.findUnique({
-      where: {
-        name: name,
-      },
-    });
-  }
+//     project = await prisma.project.findUnique({
+//       where: {
+//         name: name,
+//       },
+//     });
+//   }
 
-  return project;
-};
+//   return project;
+// };
 
-const storeDBData = async (
-  dayData: {
-    date: any;
-    fees: any;
-  },
-  projectId: number
-) => {
-  const day = await prisma.day.findFirst({
-    where: {
-      date: dayData.date,
-      projectId: projectId,
-    },
-  });
+// const storeDBData = async (
+//   dayData: {
+//     date: any;
+//     fees: any;
+//   },
+//   projectId: number
+// ) => {
+//   const day = await prisma.day.findFirst({
+//     where: {
+//       date: dayData.date,
+//       projectId: projectId,
+//     },
+//   });
 
-  if (day != null) {
-    const accruedRevenue = day.revenue + dayData.fees;
+//   if (day != null) {
+//     const accruedRevenue = day.revenue + dayData.fees;
 
-    await prisma.day.update({
-      where: {
-        id: day.id,
-      },
-      data: {
-        revenue: accruedRevenue,
-      },
-    });
-  } else {
-    await prisma.day.create({
-      data: {
-        date: dayData.date,
-        revenue: dayData.fees,
-        projectId: projectId,
-      },
-    });
-  }
+//     await prisma.day.update({
+//       where: {
+//         id: day.id,
+//       },
+//       data: {
+//         revenue: accruedRevenue,
+//       },
+//     });
+//   } else {
+//     await prisma.day.create({
+//       data: {
+//         date: dayData.date,
+//         revenue: dayData.fees,
+//         projectId: projectId,
+//       },
+//     });
+//   }
 
-  // update project's last updated date
-  await prisma.project.updateMany({
-    where: {
-      name: coin.name,
-    },
-    data: {
-      lastImportedId: dayData.date.toString(),
-    },
-  });
+//   // update project's last updated date
+//   await prisma.project.updateMany({
+//     where: {
+//       name: coin.name,
+//     },
+//     data: {
+//       lastImportedId: dayData.date.toString(),
+//     },
+//   });
 
-  return;
-};
+//   return;
+// };
 
 const getPOKTDayPrices = async (dateFrom: string, dateTo: string) => {
   const dayPrices: DayPrice[] = [];
@@ -288,3 +301,5 @@ pocketImport()
     console.log(err);
     process.exit(1);
   });
+
+export {};
